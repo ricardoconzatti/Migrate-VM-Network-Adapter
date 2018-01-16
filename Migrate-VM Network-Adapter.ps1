680072aa7e0a8bb419a9e3a179e830a76f5a2b9a
Ricardo Conzatti <ricardoconzatti@hotmail.com>
2018-01-16 11:39:13 -0200

    Version 2.0
    Support to just one VM and virtual network adapter E1000E

--- Migrate-VM Network-Adapter.ps1
+++ Migrate-VM Network-Adapter.ps1
@@ -1,39 +1,47 @@
 ######################################################################
 # Created By @RicardoConzatti | June 2017
+# Latest update: January 2018
 # www.Solutions4Crowds.com.br
 ######################################################################
-$vCenter = 0#"lab-vcsa148.s4c.local" # Default = 0
-$vCuser = 0#"administrator@vsphere.local" # Default = 0
+$vCenter = "vcenter65.servicecwb.local"#"lab-vcsa148.s4c.local" # Default = 0
+$vCuser = "administrator@vsphere.local"#"administrator@vsphere.local" # Default = 0
 ######################################################################
 cls
-$S4Ctitle = "Migrate Virtual Machine Network Adapter v1.0"
+$S4Ctitle = "Migrate Virtual Machine Network Adapter v2.0"
 $Body = 'www.Solutions4Crowds.com.br
 =======================================================
 '
 write-host $S4Ctitle
 write-host $Body
 write-host "Connect to vCenter Server`n`n=======================================================`n"
-if ($vCenter -eq 0) {
-	$vCenter = read-host "vCenter Server (FQDN or IP)"
+$vCenterConn = $global:DefaultVIServers
+if ($vCenterConn.Count -eq 1) {
+	write-host "You are connected to"$vCenterConn.Name"`n"
+	pause
 }
 else {
-	write-host "vCenter Server: $vCenter"
+	if ($vCenter -eq 0) {
+		$vCenter = read-host "vCenter Server (FQDN or IP)"
+	}
+	else {
+		write-host "vCenter Server: $vCenter"
+	}
+	if ($vCuser -eq 0) {
+		$vCuser = read-host "`nUsername"
+	}
+	else {
+		write-host "`nUsername: $vCuser"
+	}
+	$vCpass = Read-Host -assecurestring "`nPassword"
+	$vCpass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($vCpass))
+	Connect-VIServer $vCenter -u $vCuser -password $vCpass | Out-Null
+	write-host "`nConnected to $vCenter`n" -foregroundcolor "green"
+	pause
 }
-if ($vCuser -eq 0) {
-	$vCuser = read-host "`nUsername"
-}
-else {
-	write-host "`nUsername: $vCuser"
-}
-$vCpass = Read-Host -assecurestring "`nPassword"
-$vCpass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($vCpass))
-Connect-VIServer $vCenter -u $vCuser -password $vCpass | Out-Null
-write-host "`nConnected to $vCenter`n" -foregroundcolor "green"
-pause
 cls
 write-host $S4Ctitle
 write-host $Body
-write-host "Migrate Network Adapter - E1000/E1000E to VMXNET3 ($vCenter)`n`n=======================================================`n"
+write-host "Migrate Network Adapter - E1000 / E1000E to VMXNET3 ($vCenter)`n`n=======================================================`n"
 write-host "1 - Datacenter`n2 - Cluster`n3 - Resource Pool`n4 - Folder`n"
 $QuestionLocation = read-host "Select Virtual Machines Location"
 
@@ -105,9 +113,11 @@ if ($QuestionLocation -eq 4) { # Folder
 		$VMLocation = $GetFolder.Name[$MyFolder]
 	}
 }
+$VMe1000total = 0 # reset variable
+$VMpowerstate = 0 # reset variable
 $VMe1000total = Get-VM -Location $VMLocation | Get-NetworkAdapter | Where {$_.Type -eq "E1000" -Or $_.Type -eq "E1000E"}
-if ($VMe1000total.Count -eq 0) { # Check if there are VM E1000 or E1000E
-	write-host "`nThere are no E1000/E1000E virtual machines in the $VMLocation`nSelect another Virtual Machines Location`n"
+if ($VMe1000total.Count -eq 0) { # Check if there are VM e1000 / e1000e
+	write-host "`nThere are no E1000 / E1000E virtual machines in the $VMLocation`nSelect another Virtual Machines Location`n"
 	exit
 }
 else {
@@ -115,10 +125,8 @@ else {
 	$VMNum = $MyVM.Count
 	$VMNumTotal = 0
 	write-host "`n=======================================================`n$VMLocation - Total number of VM: $VMNum`n=======================================================`n"
-	write-host "Ready = E1000 or E1000E virtual machines that are not running" -foregroundcolor "green"
-	write-host "Not Ready = E1000 or E1000E virtual machines that are running`n" -foregroundcolor "red"
 	if ($VMNum -eq 1) {
-		$VMe1000 = Get-NetworkAdapter -VM $MyVM.Name | Where { $_.Type -eq "E1000" -Or $_.Type -eq "E1000E"} | sort-object
+		$VMe1000 = Get-NetworkAdapter -VM $MyVM.Name | Where {$_.Type -eq "E1000" -Or $_.Type -eq "E1000E"} | sort-object
 		if ($VMe1000.count -gt 0) {
 				if ($MyVM.PowerState -eq "PoweredOn") { # Verifies that the VM is turned on
 					$VMStatus = "red" 
@@ -135,7 +143,7 @@ else {
 	}
 	else {
 		while ($VMNum -ne $VMNumTotal) { # List all VMs in the $VMLocation
-				$VMe1000 = Get-NetworkAdapter -VM $MyVM.Name[$VMNumTotal] | Where { $_.Type -eq "E1000"} | sort-object
+				$VMe1000 = Get-NetworkAdapter -VM $MyVM.Name[$VMNumTotal] | Where {$_.Type -eq "E1000" -Or $_.Type -eq "E1000E"} | sort-object
 				if ($VMe1000.count -gt 0) {
 					if ($MyVM.PowerState[$VMNumTotal] -eq "PoweredOn") { # Verifies that the VM is turned on
 						$VMStatus = "red" 
@@ -147,36 +155,43 @@ else {
 						$VMmsg = "Ready"
 						$VMofftemp = 1;$VMoff = $VMofftemp + $VMoff
 					}
-					write-host $VMNumTotal $MyVM.Name[$VMNumTotal]"- $VMmsg" -foregroundcolor "$VMStatus"
+					write-host $MyVM.Name[$VMNumTotal]"$VMmsg" -foregroundcolor "$VMStatus"
 				}
 				$VMNumTotal++
 		}
 	}
 	$VMpowerstate = $VMoff + $VMon
-	write-host "`nTotal number of E1000/E1000E VM: $VMpowerstate"
+	write-host "`nTotal number of E1000 / E1000E VM: $VMpowerstate"
 	if ($VMoff -gt 0 -And $VMon -le 0) {
 		write-host "`n[ You can continue ]`n" -foregroundcolor "green"
-		write-host "VM they are ready: $VMoff"
+		write-host "VM are ready: $VMoff"
 	}
 	else {
 		write-host "`n[ You can't continue ]`n" -foregroundcolor "red"
 		write-host "VM must be turned off: $VMon"
-		write-host "VM they are ready: $VMoff"
+		write-host "VM are ready: $VMoff"
 		exit
 	}
 	write-host "`n=======================================================`n"
 	$QuestionMigration = read-host "Do you want to continue? (Y or N)"
 	if ($QuestionMigration -eq "Y") {
-		$VMNumTotal = 0
 		write-host
-		while ($VMNum -ne $VMNumTotal) {
-			$VMe1000 = Get-NetworkAdapter -VM $MyVM.Name[$VMNumTotal] | Where { $_.Type -eq "E1000"} | sort-object
-			if ($VMe1000.count -gt 0) {
-				Get-VM $MyVM.Name[$VMNumTotal] -Location $VMLocation | Get-NetworkAdapter | Where { $_.Type -eq "E1000"} | Set-NetworkAdapter -Type "vmxnet3" -confirm:$false | Out-Null # migrate e1000 to vmxnet3
-				write-host $MyVM.Name[$VMNumTotal]"changed to VMXNET 3"
+		if ($VMpowerstate.Count -eq 1) { # just one VM
+			Get-VM $MyVM.Name -Location $VMLocation | Get-NetworkAdapter | Where {$_.Type -eq "E1000" -Or $_.Type -eq "E1000E"} | Set-NetworkAdapter -Type "vmxnet3" -confirm:$false | Out-Null # migrate e1000 / e1000e to vmxnet3
+				write-host $MyVM.Name"changed to VMXNET 3"
+		}
+		else {
+			$VMNumTotal = 0
+			while ($VMNum -ne $VMNumTotal) { # multiples VMs
+				$VMe1000 = Get-NetworkAdapter -VM $MyVM.Name[$VMNumTotal] | Where {$_.Type -eq "E1000" -Or $_.Type -eq "E1000E"} | sort-object
+				if ($VMe1000.count -gt 0) {
+					Get-VM $MyVM.Name[$VMNumTotal] -Location $VMLocation | Get-NetworkAdapter | Where {$_.Type -eq "E1000" -Or $_.Type -eq "E1000E"} | Set-NetworkAdapter -Type "vmxnet3" -confirm:$false | Out-Null # migrate e1000 / e1000e to vmxnet3
+					write-host $MyVM.Name[$VMNumTotal]"changed to VMXNET 3"
+				}
+				$VMNumTotal++;
 			}
-			$VMNumTotal++;
 		}
 		write-host "`nMigration OK!`n"
 	}
+	Disconnect-VIServer -Server $vCenter -Confirm:$false # Disconnect vCenter
 }
